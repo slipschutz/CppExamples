a6ced4e3ecd541f0f609ad06d339d42f9f834e14
Samuel Lipschutz <Sam@unknown001F5BBF4CAC.gateway.pace.com>
2014-02-18 22:25:31 -0500

    First Commmit.
    Just have a basic Object Example

---
+++ Ex1_Objects.cpp
@@ -0,0 +1,35 @@
+#include <iostream> //Standard Library input output classes and methods
+using namespace std;  //Use the standard Namespace
+
+class Student {
+public:
+  Student(); //Constructor
+  ~Student();//Deconstructor
+  void DoHomeWork();
+  int GetHealth(){ return theHealth;}
+private:
+  int theHealth;
+
+};//Class Definition always ends with ;
+
+Student::Student(){
+  cout<<"Making a Student"<<endl;
+  theHealth=100;//Defualt Start Value for Health
+}
+Student::~Student(){
+  cout<<"Destroying a Student"<<endl;
+}
+
+void Student::DoHomeWork(){
+  cout<<"Working.."<<endl;
+  theHealth--;
+}
+int main(int argv,char ** argc){  //Always need a main
+  Student myInstanceOfTheStudentClassWhichIsAStudentObject;
+  cout<<"My Health is "<<myInstanceOfTheStudentClassWhichIsAStudentObject.GetHealth()<<endl;
+
+
+  return 0;
+}
+
+
