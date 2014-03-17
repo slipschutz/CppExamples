e6952148d4035689518a673d7162541166f84aad
Samuel Lipschutz <lipschut@pickerel.nscl.msu.edu>
2014-03-16 22:49:18 -0400

    Nothing

--- Ex2_VirtualMethods.cpp
+++ Ex2_VirtualMethods.cpp
@@ -82,11 +82,15 @@ int main(int argv,char ** argc){  //Always need a main
 
   vector <Student*> aStudentVector(2);
 
+  
 
   //WHAT?!! but what about strongly typed language
   //answer PolyMorphism
   aStudentVector[0]=new UnderGrad();
   aStudentVector[1]=new GradStudent();
+
+  
+
 
   for (int i=0;i<aStudentVector.size();i++){
     aStudentVector[i]->DoHomeWork();
